e7ecc8270c7fcea3d66feb58edfff50acb5cf65b
Habil <habil0105@gmail.com>
2024-02-11 11:46:13 +0700

    Add responsive sizing for icons and circle avatars

--- lib/shared/extensions/media_query_extension.dart
+++ lib/shared/extensions/media_query_extension.dart
@@ -4,4 +4,6 @@ import 'package:my_template/main.dart';
 extension CustomMediaQuery on num {
   double get h => MediaQuery.of(navigatorKey.currentContext!).size.height * this / 100;
   double get w => MediaQuery.of(navigatorKey.currentContext!).size.width * this / 100;
+  double get size => MediaQuery.of(navigatorKey.currentContext!).size.shortestSide * this / 100;
+  double get radius => MediaQuery.of(navigatorKey.currentContext!).size.shortestSide * this / 200;
 }
